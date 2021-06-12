557b7a233e9ce81c8e655e201a78bd096c7a1313
amr khafagy <amr.khafagy@yahoo.com>
2021-06-12 12:00:19 +0100

    init

--- api/app.js
+++ api/app.js
@@ -4,10 +4,29 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const config = require('./config');
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// mango connaction 
+MongoClient.connect(`mongodb://${config.dbHost}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(client => {
+    const db = client.db(config.dbName);
+    const collection = db.collection(config.dbCollection);    
+    app.locals[config.dbCollection] = collection;
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
